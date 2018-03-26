9a8f801761edc32930ce0cf9440bcace85b0d359
Sumit Somani <sumit.somani@mail.utoronto.ca>
2018-03-26 19:52:32 -0400

    Include moving background. Fix bugs in moveBlock

--- moveBlock.v
+++ moveBlock.v
@@ -20,10 +20,10 @@ module moveBlock(slowed_clock, block_x, block_y, ground_top, block_width, screen
             
             //if block was on ground
             if(block_y <= ground_top)
-                block_y <= block_y + y_change;
+                block_y <= block_y - y_change;   // - => above ground
             //if block was above ground
             else
-                block_y <= block_y - y_change;
+                block_y <= block_y + y_change;
         end
         else
             block_x <= block_x - 1;
@@ -33,12 +33,14 @@ module moveBlock(slowed_clock, block_x, block_y, ground_top, block_width, screen
 endmodule
 
 module randomPosition(clock, x_displacement, y_displacement);
+     
+	 input clock; 
 
-    output reg [4:0] x_position, y_position;
+    output reg [4:0] x_displacement, y_displacement;
     reg   above_ground;
     
     always @(posedge clock) begin
-        x_postion  <= $urandom%20;
+        x_displacement  <= $urandom%20;
         
         above_ground <= $urandom%2;
     
@@ -47,5 +49,35 @@ module randomPosition(clock, x_displacement, y_displacement);
             y_displacement <= 5'b0;
         else
             y_displacement <= 5'd15;
+    end
+endmodule
 
+module RanGen(
+    input               rst_n,    /*rst_n is necessary to prevet locking up*/
+    input               clk,      /*clock signal*/
+//    input               load,     /*load seed to rand_num,active high */
+    input      [7:0]    seed,     
+    output reg [7:0]    rand_num  /*random number output*/
+);
+
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(!rst_n)
+        rand_num    <= seed;
+//    else if(load)
+//        rand_num <=seed;    /*load the initial value when load is active*/
+    else
+        begin
+            rand_num[0] <= rand_num[7];
+            rand_num[1] <= rand_num[0];
+            rand_num[2] <= rand_num[1];
+            rand_num[3] <= rand_num[2];
+            rand_num[4] <= rand_num[3]^rand_num[7];
+            rand_num[5] <= rand_num[4]^rand_num[7];
+            rand_num[6] <= rand_num[5]^rand_num[7];
+            rand_num[7] <= rand_num[6];
+        end
+            
+end
 endmodule
